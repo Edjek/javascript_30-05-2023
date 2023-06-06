28755d69f8189fd0a55041699e435b4a2e4d6ed2
Edjek <edjek@hotmail.fr>
2023-06-06 17:07:06 +0200

    day 5

---
+++ js-practice/formulaire/js/main.js
@@ -0,0 +1,21 @@
+const formInscription = document.querySelector('#form-inscription')
+
+formInscription.addEventListener('submit', function(e){
+    e.preventDefault();
+    const name = document.querySelector('#name')
+    // console.log(name.value);
+    if(name.value == ''){
+    }  
+})
+// Cibler chaque champ de formulaire
+// Cibler le formulaire
+
+// Mettre un ecouteur sur le formulaire => submit
+    // Sur chaque chaque champ s'il est vide
+        // creer un element => message d'erreur
+        // Si les mot de passe correspondent    
+        // creer un element => message d'erreur
+    
+// On mettra en fonction la verification du formulaire
+
+// On mettra en fonction la création de message d'erreur
