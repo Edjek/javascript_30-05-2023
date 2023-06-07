954b6b4f42f3aaecf524593db4a95def783a712a
Edjek <edjek@hotmail.fr>
2023-06-07 17:01:28 +0200

    day 6

--- js-practice/todo-list/js/main.js
+++ js-practice/todo-list/js/main.js
@@ -1,32 +1,21 @@
-// // TODO List
-// const submit = document.querySelector('#submit')
-// const todoList = document.querySelector('#todo-list')
-
-// submit.addEventListener('click', function () {
-//     const input = document.querySelector('#todo-text')
-//     if (input.value !== '') {
-//         const li = document.createElement('li')
-//         li.innerHTML = input.value
-//         todoList.append(li)
-//         input.value = ''
-//     }
-// })
-
-// todoList.addEventListener('click', function (e) {
-//     e.target.style.textDecoration = 'line-through'
-// })
-
-// todoList.addEventListener('dblclick', function (e) {
-//     e.target.remove()
-// })
-
-
-
 // Cibler notre bouton
 // Lui mettre un ecouteur => click
     // Cibler le champs du valeur pour recuperer
     // cible ma liste
     // creer un element que je rajoute a la liste
+
+    const submit = document.querySelector('#submit')
+
+    submit.addEventListener('click', function(){
+        const input = document.querySelector('#todo-text')
+        const list = document.querySelector('#todo-list')
+        if(input.value != ''){
+            const elem = document.createElement('li')
+            elem.innerText = input.value
+            elem.classList.add('list-item')
+            list.prepend(elem)
+        }
+    })
 
 
 
