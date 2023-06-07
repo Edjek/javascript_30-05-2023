34b877d0bf365ad3b209282fc204121c1dda1bcd
Edjek <edjek@hotmail.fr>
2023-06-07 16:28:54 +0200

    day 6

---
+++ js-practice/todo-list/js/main.js
@@ -0,0 +1,56 @@
+// // TODO List
+// const submit = document.querySelector('#submit')
+// const todoList = document.querySelector('#todo-list')
+
+// submit.addEventListener('click', function () {
+//     const input = document.querySelector('#todo-text')
+//     if (input.value !== '') {
+//         const li = document.createElement('li')
+//         li.innerHTML = input.value
+//         todoList.append(li)
+//         input.value = ''
+//     }
+// })
+
+// todoList.addEventListener('click', function (e) {
+//     e.target.style.textDecoration = 'line-through'
+// })
+
+// todoList.addEventListener('dblclick', function (e) {
+//     e.target.remove()
+// })
+
+
+
+// Cibler notre bouton
+// Lui mettre un ecouteur => click
+    // Cibler le champs du valeur pour recuperer
+    // cible ma liste
+    // creer un element que je rajoute a la liste
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
