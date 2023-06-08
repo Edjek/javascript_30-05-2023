21fe605d77385903bcd38a342b920383e99d2f04
Edjek <edjek@hotmail.fr>
2023-06-08 15:28:40 +0200

    day 7

--- js-practice/todo-list/js/main.js
+++ js-practice/todo-list/js/main.js
@@ -1,6 +1,6 @@
 const submit = document.querySelector('#submit')
 const list = document.querySelector('#todo-list')
-
+console.log(list);
 submit.addEventListener('click', function () {
     const input = document.querySelector('#todo-text')
 
@@ -16,32 +16,10 @@ list.addEventListener('click', function (e) {
     // e.target permet de cibler l'element qui est réellement visé
     e.target.style.textDecoration = 'line-through'
     // e.currentTarget cible l'element courant donc celui où on a mis l'écouteur
-    e.currentTarget;
-    
+    // e.currentTarget;
+
 })
 
-// mettez un ecouteur sur la list pour ecouter le doucle click
-    // quand on double clique sur un element de la liste on le supprime
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+list.addEventListener('dblclick', function (event) {
+    event.target.remove()
+})
