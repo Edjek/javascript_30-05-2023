b31baddc5575a0c848c592625eae1d10cc45c6aa
test <test@ggmail.com>
2023-06-08 10:08:52 +0200

    js

--- js-practice/todo-list/js/main.js
+++ js-practice/todo-list/js/main.js
@@ -1,19 +1,27 @@
 const submit = document.querySelector('#submit')
+const list = document.querySelector('#todo-list')
 
-submit.addEventListener('click', function(){
+submit.addEventListener('click', function () {
     const input = document.querySelector('#todo-text')
-    const list = document.querySelector('#todo-list')
-    if(input.value != ''){
+
+    if (input.value != '') {
         const elem = document.createElement('li')
         elem.innerText = input.value
         elem.classList.add('list-item')
-        list.prepend(elem)
+        list.append(elem)
     }
 })
 
+list.addEventListener('click', function (e) {
+    // e.target permet de cibler l'element qui est réellement visé
+    e.target.style.textDecoration = 'line-through'
+    // e.currentTarget cible l'element courant donc celui où on a mis l'écouteur
+    e.currentTarget;
+    
+})
 
-
-
+// mettez un ecouteur sur la list pour ecouter le doucle click
+    // quand on double clique sur un element de la liste on le supprime
 
 
 
